07c3afa9bfb45af565758fa36334075ab28bc642
Nam Tran <nhtranngoc@wpi.edu>
2020-10-14 21:14:07 -0400

    Added markov script

--- markov.js
+++ markov.js
@@ -1,3 +1,453 @@
+names = {
+    "alethi": {
+        "male":[
+            "Abry",
+            "Acis",
+            "Adolin",
+            "Agil",
+            "Alabet",
+            "Aladar",
+            "Alds",
+            "Arafik",
+            "Aratin",
+            "Avarak",
+            "Avaran",
+            "Bashin",
+            "Beard",
+            "Beld",
+            "Bethab",
+            "Bordin",
+            "Caull",
+            "Cenn",
+            "Colot",
+            "Dalar",
+            "Dalinar",
+            "Dallet",
+            "Deedanor",
+            "Dod",
+            "Dru",
+            "Dunny",
+            "Elhokar",
+            "Elit",
+            "Febin",
+            "Febin",
+            "Galan",
+            "Gallam",
+            "Gavilar",
+            "Graves",
+            "Habrin",
+            "Halad",
+            "Halam",
+            "Hallaw",
+            "Hamadin",
+            "Harl",
+            "Harth",
+            "Hatham",
+            "Hathan",
+            "Hav",
+            "Havar",
+            "Havarah",
+            "Havrom",
+            "Hid",
+            "Idrin",
+            "Ilamar",
+            "Isasik",
+            "Jakamav",
+            "Jam",
+            "Jest",
+            "Jor",
+            "Jorna",
+            "Jost",
+            "Kadash",
+            "Kaladin",
+            "Kalanor",
+            "Kalishnor",
+            "Kelerand",
+            "Khav",
+            "Khem",
+            "Lamaril",
+            "Lanacin",
+            "Lirin",
+            "Loats",
+            "Loradar",
+            "Lustow",
+            "Mabrow",
+            "Malan",
+            "Marf",
+            "Marks",
+            "Matain",
+            "Matal",
+            "Meleran",
+            "Meridas",
+            "Milp",
+            "Moash",
+            "Mord",
+            "Nacomb",
+            "Naget",
+            "Natam",
+            "Natir",
+            "Niter",
+            "Noromin",
+            "Oroden",
+            "Paladar",
+            "Perethom",
+            "Ral",
+            "Ralinor",
+            "Rembrinor",
+            "Renarin",
+            "Resi",
+            "Rial",
+            "Rillir",
+            "Rind",
+            "Roion",
+            "Sadeas",
+            "Sadees",
+            "Salinor",
+            "Serugiadis",
+            "Saday",
+            "Sheler",
+            "Shim",
+            "Sidin",
+            "Taka",
+            "Talanor",
+            "Taleb",
+            "Tanalan",
+            "Taselin",
+            "Tearim",
+            "Teft",
+            "Teleb",
+            "Thakka",
+            "Thanadal",
+            "Tibon",
+            "Tien",
+            "Tinalar",
+            "Ton",
+            "Torol",
+            "Tukks",
+            "Turinad",
+            "Urimil",
+            "Vaceslv",
+            "Valama",
+            "Vardinar",
+            "Vartian",
+            "Vedekar",
+            "Velalant",
+            "Ven",
+            "Vevidar",
+            "Wistiow",
+            "Yenev",
+            "Yezriar"
+        ],
+        "female": [
+            "Aesudan",
+            "Alami",
+            "Chanada",
+            "Danlan",
+            "Eshava",
+            "Hamaradin",
+            "Hashal",
+            "Hesina",
+            "Ialai",
+            "Inadara",
+            "Inkima",
+            "Ishal",
+            "Ishnah",
+            "Istow",
+            "Ithi",
+            "Ivis",
+            "Janala",
+            "Jasalai",
+            "Jasnah",
+            "Jayla",
+            "Jenet",
+            "Jevena",
+            "Kusiri",
+            "Lalai",
+            "Laral",
+            "Litima",
+            "Lyn",
+            "Mara",
+            "Miasal",
+            "Muri",
+            "Nall",
+            "Nananav",
+            "Navani",
+            "Relinar",
+            "Ristina",
+            "Rushu",
+            "Sani",
+            "Seland",
+            "Tarah",
+            "Terith",
+            "Teshav",
+            "Velat"
+        ],
+        "surname": [
+            "Amaram",
+            "Eved",
+            "Gaval",
+            "Khal",
+            "Kholin",
+            "Marakal",
+            "Morakotha",
+            "Perel",
+            "Roshone",
+            "Ruthar",
+            "Sebarial",
+            "Shulin",
+            "Vamah"
+        ],
+        "glyph": [
+            "jeseh",
+            "nan",
+            "chach",
+            "vev",
+            "palah",
+            "shash",
+            "beteb",
+            "kak",
+            "tanat",
+            "ishi",
+            "abara",
+            "adoda",
+            "amaya",
+            "bakhash",
+            "gesheh",
+            "idi",
+            "javani",
+            "kadulek",
+            "kalak",
+            "katef",
+            "kecheh",
+            "khakh",
+            "khohk",
+            "markhazh",
+            "laial",
+            "lerel",
+            "linil",
+            "mehlak",
+            "merem",
+            "mevizh",
+            "morom",
+            "nahn",
+            "parap",
+            "sas",
+            "sebes",
+            "shash",
+            "shenesh",
+            "tafar",
+            "tebel",
+            "thath",
+            "tsameth",
+            "zatalef",
+            "zeras"
+        ]
+    },
+    "singers": {
+        "male": [
+            "Abranay",
+            "Abronai",
+            "Aharat",
+            "Altoki",
+            "Bru",
+            "Davim",
+            "Demid",
+            "Devi",
+            "Gitgeth",
+            "Jali",
+            "Klade",
+            "Kyril",
+            "Mrun",
+            "Nam",
+            "Rine",
+            "Sah",
+            "Thude",
+            "Ton",
+            "Turash",
+            "Varanis"
+        ],
+        "female": [
+            "Bila",
+            "Brianlia",
+            "Chivi",
+            "Eshonai",
+            "Gangnah",
+            "Hariel",
+            "Hesh",
+            "Hnanan",
+            "Khen",
+            "Leshwi",
+            "Melu",
+            "Pal",
+            "Rlain",
+            "Vai",
+            "Varnali",
+            "Vatwha",
+            "Venli",
+            "Vldgen",
+            "Zuln"
+        ]
+    },
+    "veden": {
+        "male": [
+            "Abrial",
+            "Amark",
+            "Balat",
+            "Beal",
+            "Boriar",
+            "Ekel",
+            "Evinor",
+            "Geved",
+            "Gevelmar",
+            "Hanavanar",
+            "Helaran",
+            "Jal",
+            "Jix",
+            "Jushu",
+            "Lin",
+            "Luesh",
+            "Mill",
+            "Nalanar",
+            "NanHar",
+            "NanKhet",
+            "Redin",
+            "Ren",
+            "Revilar",
+            "Rin",
+            "Sur",
+            "Talak",
+            "Ten",
+            "Ton",
+            "Urian",
+            "Urv",
+            "Wikim"
+        ],
+        "female": [
+            "Eylita",
+            "Hasheh",
+            "Malata",
+            "Malise",
+            "Mem",
+            "Natata Ved",
+            "Shallan",
+            "Sudi"
+        ],
+        "surname": [
+            "Davar",
+            "Tavinar",
+            "Valam",
+            "Kamar"
+        ]
+    },
+    "thaylen": {
+        "male": [
+            "Artmyrn",
+            "Barmest",
+            "Beznk",
+            "Estnatil",
+            "Febrth",
+            "Fladm",
+            "Hrdalm",
+            "Jochi",
+            "Kdralk",
+            "Kmakl",
+            "Mraize",
+            "Mrall",
+            "Tlik",
+            "Tozbek",
+            "Tshadr",
+            "Tvlakv",
+            "Vazrmeb",
+            "Vstim",
+            "Wzmal",
+            "Yalb",
+            "Yokska"
+        ],
+        "female": [
+            "Ashlv",
+            "Devmrh",
+            "Fen",
+            "Kmakra",
+            "Rysn"
+        ]
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Create corpus
+    for(const key of Object.keys(names)) {
+        console.log(key)
+    }
+}, false);
+
+function generate_corpus(names) {
+    let corpus = [];
+
+    names.forEach((el, ar) => {
+        el += '\n';
+        corpus.push.apply(corpus, el.split(''));
+    });
+
+    return corpus;
+}
+
+function generate_chain(corpus) {
+    let chain = {};
+    let n_names = corpus.length;
+
+    corpus.forEach((key1, i) => {
+        let key2 = '';
+        let letter = '';
+        if (n_names > i + 2) {
+            key2 = corpus[i + 1];
+            letter = corpus[i + 2];
+        }
+
+        let key = key1 + key2;
+        if (!chain[key]) {
+            chain[key] = [letter];
+        } else {
+            chain[key].push(letter)
+        }
+    });
+
+    return chain;
+}
+
+function markov(corpus, chain) {
+    let nkey = '  ';
+    while(
+        nkey.charAt(0) === ' ' ||
+        nkey.charAt(0) === nkey.charAt(0).toLowerCase() ||
+        nkey.charAt(0) === '\n' ||
+        !chain[nkey]
+    ) {
+        let key1 = corpus[Math.floor(Math.random() * corpus.length)];
+        let key2 = corpus[Math.floor(Math.random() * corpus.length)];
+        nkey = key1 + key2;
+    }
+
+    let l = '';
+    let name = nkey;
+    while (l !== '\n') {
+        l = chain[nkey][Math.floor(Math.random() * chain[nkey].length)];
+        name += l;
+        nkey = nkey.charAt(1) + l;
+    }
+
+    // Take off carriage return
+    return name.slice(0, -1);
+
+}
+
 function generate() {
-    console.log("AAA");
+    let search = names.alethi.glyph.concat(names.alethi.male);
+    let corpus = generate_corpus(search);
+    let chain = generate_chain(corpus);
+    let name = markov(corpus, chain);
+
+    console.log(name);
+    if (search.includes(name)) {
+        console.log("Hey this is in the book!");
+    }
 }
